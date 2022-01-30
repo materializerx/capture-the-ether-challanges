494c478e508e0915fb23efbe88fd67f196fbd11d
komdori <wonderkidd2022@gmail.com>
2022-01-29 23:20:27 -0400

    fix test

--- test/miscellaneous/AssumeOwnership.ts
+++ test/miscellaneous/AssumeOwnership.ts
@@ -2,7 +2,6 @@ import crypto, { sign } from "crypto";
 import { ethers } from "hardhat";
 import { BigNumber, Contract, Signer } from "ethers";
 import { expect } from "chai";
-// import { formatEtherscanTx } from "../utils/format";
 import { HDNode } from "ethers/lib/utils";
 
 let accounts: Signer[];
@@ -16,18 +15,18 @@ before(async () => {
   const challengeFactory = await ethers.getContractFactory(
     "AssumeOwnershipChallenge"
   );
-  challengeContract = challengeFactory.attach(
-    `0x139D666C0ffE1F9ba6f557D57E35ddB198D6ef88`
-  );
+  // challengeContract = challengeFactory.attach(
+  //   `0x139D666C0ffE1F9ba6f557D57E35ddB198D6ef88`
+  // );
 });
 
 it("solves the challenge", async function () {
   // the function looks like a constructor but it is misspelled
   // so we can call it directly
-  tx = await challengeContract.AssumeOwmershipChallenge()
-  await tx.wait()
-  tx = await challengeContract.authenticate();
-  await tx.wait();
+  // tx = await challengeContract.AssumeOwmershipChallenge()
+  // await tx.wait()
+  // tx = await challengeContract.authenticate();
+  // await tx.wait();
 
   const isComplete = await challengeContract.isComplete();
   expect(isComplete).to.be.true;
