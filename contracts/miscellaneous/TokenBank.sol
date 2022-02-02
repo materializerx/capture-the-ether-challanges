30ccbf71aa92a4f29cf226c979ad4cc53b309382
komdori <wonderkidd2022@gmail.com>
2022-02-01 23:54:37 -0400

    [WIP] test TokenBank with other version of contract

--- contracts/miscellaneous/TokenBank.sol
+++ contracts/miscellaneous/TokenBank.sol
@@ -3,78 +3,79 @@ pragma solidity ^0.4.21;
 interface ITokenReceiver {
     function tokenFallback(address from, uint256 value, bytes data) external;
 }
+// replace and test with other version
 
-contract SimpleERC223Token {
-    // Track how many tokens are owned by each address.
-    mapping (address => uint256) public balanceOf;
+// contract SimpleERC223Token {
+//     // Track how many tokens are owned by each address.
+//     mapping (address => uint256) public balanceOf;
 
-    string public name = "Simple ERC223 Token";
-    string public symbol = "SET";
-    uint8 public decimals = 18;
+//     string public name = "Simple ERC223 Token";
+//     string public symbol = "SET";
+//     uint8 public decimals = 18;
 
-    uint256 public totalSupply = 1000000 * (uint256(10) ** decimals);
+//     uint256 public totalSupply = 1000000 * (uint256(10) ** decimals);
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+//     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    function SimpleERC223Token() public {
-        balanceOf[msg.sender] = totalSupply;
-        emit Transfer(address(0), msg.sender, totalSupply);
-    }
+//     function SimpleERC223Token() public {
+//         balanceOf[msg.sender] = totalSupply;
+//         emit Transfer(address(0), msg.sender, totalSupply);
+//     }
 
-    function isContract(address _addr) private view returns (bool is_contract) {
-        uint length;
-        assembly {
-            //retrieve the size of the code on target address, this needs assembly
-            length := extcodesize(_addr)
-        }
-        return length > 0;
-    }
+//     function isContract(address _addr) private view returns (bool is_contract) {
+//         uint length;
+//         assembly {
+//             //retrieve the size of the code on target address, this needs assembly
+//             length := extcodesize(_addr)
+//         }
+//         return length > 0;
+//     }
 
-    function transfer(address to, uint256 value) public returns (bool success) {
-        bytes memory empty;
-        return transfer(to, value, empty);
-    }
+//     function transfer(address to, uint256 value) public returns (bool success) {
+//         bytes memory empty;
+//         return transfer(to, value, empty);
+//     }
 
-    function transfer(address to, uint256 value, bytes data) public returns (bool) {
-        require(balanceOf[msg.sender] >= value);
+//     function transfer(address to, uint256 value, bytes data) public returns (bool) {
+//         require(balanceOf[msg.sender] >= value);
 
-        balanceOf[msg.sender] -= value;
-        balanceOf[to] += value;
-        emit Transfer(msg.sender, to, value);
+//         balanceOf[msg.sender] -= value;
+//         balanceOf[to] += value;
+//         emit Transfer(msg.sender, to, value);
 
-        if (isContract(to)) {
-            ITokenReceiver(to).tokenFallback(msg.sender, value, data);
-        }
-        return true;
-    }
+//         if (isContract(to)) {
+//             ITokenReceiver(to).tokenFallback(msg.sender, value, data);
+//         }
+//         return true;
+//     }
 
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+//     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    mapping(address => mapping(address => uint256)) public allowance;
+//     mapping(address => mapping(address => uint256)) public allowance;
 
-    function approve(address spender, uint256 value)
-        public
-        returns (bool success)
-    {
-        allowance[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
-        return true;
-    }
+//     function approve(address spender, uint256 value)
+//         public
+//         returns (bool success)
+//     {
+//         allowance[msg.sender][spender] = value;
+//         emit Approval(msg.sender, spender, value);
+//         return true;
+//     }
 
-    function transferFrom(address from, address to, uint256 value)
-        public
-        returns (bool success)
-    {
-        require(value <= balanceOf[from]);
-        require(value <= allowance[from][msg.sender]);
+//     function transferFrom(address from, address to, uint256 value)
+//         public
+//         returns (bool success)
+//     {
+//         require(value <= balanceOf[from]);
+//         require(value <= allowance[from][msg.sender]);
 
-        balanceOf[from] -= value;
-        balanceOf[to] += value;
-        allowance[from][msg.sender] -= value;
-        emit Transfer(from, to, value);
-        return true;
-    }
-}
+//         balanceOf[from] -= value;
+//         balanceOf[to] += value;
+//         allowance[from][msg.sender] -= value;
+//         emit Transfer(from, to, value);
+//         return true;
+//     }
+// }
 
 contract TokenBankChallenge {
     SimpleERC223Token public token;
